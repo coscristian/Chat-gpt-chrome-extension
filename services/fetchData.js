5bdd91ee24d7397a473ba5975f8b743b367d9a97
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-05-17 11:31:25 -0500

    Add documentation to getChatGptResponse function

--- services/fetchData.js
+++ services/fetchData.js
@@ -7,14 +7,18 @@ var options = {
 };
 
 class FetchData {
+  /**
+   * Make the request to the specified URL
+   * @returns {JSON} The response from the API
+   */
   async getChatGptResponse() {
     return await axios
       .request(options)
       .then(function (response) {
-            return response.data;
+        return response.data;
       })
       .catch(function (error) {
-            return error;
+        return error;
       });
   }
 }
