55cff591ce7cc45cafec98394ca2ea7b975b45a1
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-05-18 10:12:32 -0500

    doc: Add documentation
    Comments in important queries to get easy understanding of its functionality

--- popup.js
+++ popup.js
@@ -1,11 +1,13 @@
 /**
  * Make the request to the specified apiUrl
  * @param {String} userPrompt The user message to send to the API
- * @returns {JSON} The response from the API
+ * @returns {Promise} The response from the API
  */
 async function sendUserQuestion(userPrompt) {
-  const apiKey = "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";
+  const apiKey = "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";
   const apiUrl = "https://api.openai.com/v1/completions";
+
+  // Make a POST request to the OpenAI API
   return fetch(apiUrl, {
     method: "POST",
     headers: {
@@ -20,9 +22,10 @@ async function sendUserQuestion(userPrompt) {
     }),
   })
     .then(function (response) {
+      // Parse the response as JSON and return it
       return response.json();
     })
-    .catch((error) => console.log("Error " + error));
+    .catch((error) => Promise.reject(error));
 }
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -30,19 +33,26 @@ document.addEventListener("DOMContentLoaded", function () {
   var chatAnsElement = document.getElementById("chatAns");
   chatAnsElement.style.display = "none";
 
+  // Add click event listener to the button
   btn.addEventListener("click", function () {
     textAreaElement = document.getElementById("userQuestion");
 
+    // Send the user's question to the API and handle the response
     sendUserQuestion(textAreaElement.value)
       .then((response) => {
         var chatAns = response.choices[0].text;
+
+        // Hide the user's question input and display the answer
+        document.getElementById("userQuestion").style.display = "none";
         chatAnsElement.style.display = "block";
 
+        // Create a text node with the answer and append it to the answer element
         var textNode = document.createTextNode(chatAns);
         chatAnsElement.appendChild(textNode);
       })
-      .catch((error) => console.log("Error " + error));
-
-    document.getElementById("userQuestion").style.display = "none";
+      .catch((error) => {
+        // Handle any errors that occur during the request
+        alert("An error has ocurred, please try again later. " + error);
+      });
   });
 });
