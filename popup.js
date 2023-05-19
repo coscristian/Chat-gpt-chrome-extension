ca468abb7abd00358cb03daada9f841884373e22
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-05-19 00:14:13 -0500

    feat: Add button to refresh
    Refresh allow users to add a new prompt

--- popup.js
+++ popup.js
@@ -1,10 +1,12 @@
+
+
 /**
  * Make the request to the specified apiUrl
  * @param {String} userPrompt The user message to send to the API
  * @returns {Promise} The response from the API
  */
 async function sendUserQuestion(userPrompt) {
-  const apiKey = "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";
+  const apiKey = config.SECRET_KEY;
   const apiUrl = "https://api.openai.com/v1/completions";
 
   // Make a POST request to the OpenAI API
@@ -28,26 +30,44 @@ async function sendUserQuestion(userPrompt) {
     .catch((error) => Promise.reject(error));
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  var btn = document.getElementById("btn");
+function clearChat() {
   var chatAnsElement = document.getElementById("chatAns");
-  chatAnsElement.style.display = "none";
+  chatAnsElement.innerHTML = "";
+}
+
+function initUI(){
+    document.getElementById("userQuestion").style.display = "block";
+    document.getElementById("chatAns").style.display = "none";
+    document.getElementById("userQuestion").value = "";
+    document.getElementById("refresh").style.display = "none";
+    document.getElementById("btn").style.display = "block";
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  var searchBtn = document.getElementById("btn");
+  var refreshBtn = document.getElementById("refresh");
+
+  initUI();
 
   // Add click event listener to the button
-  btn.addEventListener("click", function () {
+  searchBtn.addEventListener("click", function () {
     textAreaElement = document.getElementById("userQuestion");
 
     // Send the user's question to the API and handle the response
     sendUserQuestion(textAreaElement.value)
       .then((response) => {
         var chatAns = response.choices[0].text;
+        var chatAnsElement = document.getElementById("chatAns");
 
         // Hide the user's question input and display the answer
         document.getElementById("userQuestion").style.display = "none";
         chatAnsElement.style.display = "block";
+        searchBtn.style.display = "none";
+        document.getElementById("refresh").style.display = "block";
 
         // Create a text node with the answer and append it to the answer element
         var textNode = document.createTextNode(chatAns);
+        chatAnsElement.innerHTML = "";
         chatAnsElement.appendChild(textNode);
       })
       .catch((error) => {
@@ -55,4 +75,9 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("An error has ocurred, please try again later. " + error);
       });
   });
+
+  refreshBtn.addEventListener("click", function () {    
+    initUI();
+  });
+
 });
